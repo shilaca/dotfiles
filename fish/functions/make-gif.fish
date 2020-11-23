0a45ed145c8c4c9ed9f318e0416dc9d8774f9d73
shilaca <shilaca.fu@gmail.com>
2020-11-23 21:45:42 +0900

    fix: 🐛 fix make-gif command

--- fish/functions/make-gif.fish
+++ fish/functions/make-gif.fish
@@ -2,6 +2,6 @@ function make-gif
   mkdir -p dist-gif && \
   cd dist-gif && \
   ffmpeg -i ../$argv[1] -vf "palettegen" -y palette.png && \
-  ffmpeg -i ../$argv[1] -i palette.png -lavfi "fps=12,scale=900:-1:flags=lanczos [x]; [x][1:v] paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" -y $1.gif && \
+  ffmpeg -i ../$argv[1] -i "palette.png" -lavfi "fps=12,scale=900:-1:flags=lanczos [x]; [x][1:v] paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" -y "$argv[2].gif" && \
   cd ..
 end
