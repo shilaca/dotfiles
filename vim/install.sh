2eceb042cb5a98b4726f53b3008003ddbf2dd0bd
shilace <shilace.fu@gmail.com>
2021-02-08 10:49:24 +0900

    refactor: 💡 vim conf

--- vim/install.sh
+++ vim/install.sh
@@ -12,3 +12,8 @@ fi
 if [ ! -e $HOME/.vimrc ]; then
     ln -snfv $PWD/.vimrc $HOME/.vimrc
 fi
+
+if [ ! -e $path/coc-settings.json ]; then
+    ln -snfv $PWD/coc-settings.json $path/coc-settings.json
+fi
+
